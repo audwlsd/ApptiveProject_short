8e884ce66f34d249bca1449f100db5fe0511ce3c
audwlsd <audwlsd7782@naver.com>
2019-05-10 15:58:41 +0900

    별명 짓기 큰틀 완성

---
+++ app/src/main/java/com/example/audwl/myapplication/NickName.java
@@ -0,0 +1,58 @@
+package com.example.audwl.myapplication;
+
+public class NickName {
+    public static String[] array_111 ={"사랑꾼","사랑돌이"};
+    public static String[] array_112 ={"daf"};
+    public static String[] array_113 ={"daf"};
+    public static String[] array_114 ={"daf"};
+    public static String[] array_121 ={"daf"};
+    public static String[] array_122 ={"daf"};
+    public static String[] array_123 ={"daf"};
+    public static String[] array_124 ={"daf"};
+    public static String[] array_131 ={"daf"};
+    public static String[] array_132 ={"daf"};
+    public static String[] array_133 ={"daf"};
+    public static String[] array_134 ={"daf"};
+    public static String[] array_141 ={"daf"};
+    public static String[] array_142 ={"daf"};
+    public static String[] array_143 ={"daf"};
+    public static String[] array_144 ={"daf"};
+
+
+    public static String[] array_211 ={"daf"};
+    public static String[] array_212 ={"daf"};
+    public static String[] array_213 ={"daf"};
+    public static String[] array_214 ={"daf"};
+    public static String[] array_221 ={"daf"};
+    public static String[] array_222 ={"daf"};
+    public static String[] array_223 ={"daf"};
+    public static String[] array_224 ={"daf"};
+    public static String[] array_231 ={"daf"};
+    public static String[] array_232 ={"daf"};
+    public static String[] array_233 ={"daf"};
+    public static String[] array_234 ={"daf"};
+    public static String[] array_241 ={"daf"};
+    public static String[] array_242 ={"daf"};
+    public static String[] array_243 ={"daf"};
+    public static String[] array_244 ={"daf"};
+
+
+    public static String[] array_311 ={"daf"};
+    public static String[] array_312 ={"daf"};
+    public static String[] array_313 ={"daf"};
+    public static String[] array_314 ={"daf"};
+    public static String[] array_321 ={"daf"};
+    public static String[] array_322 ={"daf"};
+    public static String[] array_323 ={"daf"};
+    public static String[] array_324 ={"daf"};
+    public static String[] array_331 ={"daf"};
+    public static String[] array_332 ={"daf"};
+    public static String[] array_333 ={"daf"};
+    public static String[] array_334 ={"daf"};
+    public static String[] array_341 ={"daf"};
+    public static String[] array_342 ={"daf"};
+    public static String[] array_343 ={"daf"};
+    public static String[] array_344 ={"daf"};
+
+
+}
--- app/src/main/java/com/example/audwl/myapplication/sub4Activity.java
+++ app/src/main/java/com/example/audwl/myapplication/sub4Activity.java
@@ -1,13 +1,88 @@
 package com.example.audwl.myapplication;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.audwl.myapplication.databinding.*;
+
+import java.util.Random;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static com.example.audwl.myapplication.MainActivity.getGlobalValue;
+import static com.example.audwl.myapplication.NickName.array_111;
 
 public class sub4Activity extends AppCompatActivity {
+    ActivitySub4Binding binding;
+
+    @BindView(R.id.text4)
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sub4);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sub4);
+        ButterKnife.bind(this);
+        Random generator = new Random();
+
+
+        int random_number =generator.nextInt(2);
+        switch (getGlobalValue()){
+            case 111:
+                binding.text4.setText(array_111[random_number]);
+                break;
+            case 112:
+            case 113:
+            case 114:
+            case 121:
+            case 122:
+            case 123:
+            case 124:
+            case 131:
+            case 132:
+            case 133:
+            case 134:
+            case 141:
+            case 142:
+            case 143:
+            case 144:
+
+            case 211:
+            case 212:
+            case 213:
+            case 214:
+            case 221:
+            case 222:
+            case 223:
+            case 224:
+            case 231:
+            case 232:
+            case 233:
+            case 234:
+            case 241:
+            case 242:
+            case 243:
+            case 244:
+
+            case 311:
+            case 312:
+            case 313:
+            case 314:
+            case 321:
+            case 322:
+            case 323:
+            case 324:
+            case 331:
+            case 332:
+            case 333:
+            case 334:
+            case 341:
+            case 342:
+            case 343:
+            case 344:
+        }
     }
 }
